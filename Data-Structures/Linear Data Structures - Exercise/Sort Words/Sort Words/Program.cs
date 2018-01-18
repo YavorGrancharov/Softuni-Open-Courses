5ddadad0bc2e091011827fdc3520770c04054aa7
Yavor Grancharov <bearhold@gmail.com>
2018-01-18 22:46:34 +0200

    added new folder to repo

---
+++ Data-Structures/Linear Data Structures - Exercise/Sort Words/Sort Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split(' ').ToList();
+
+            Console.WriteLine(string.Join(" ", words.OrderBy(x => x)));
+        }
+    }
+}
