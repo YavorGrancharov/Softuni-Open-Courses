ed8c3b3625ebab5e505eee3888cb0940cbeb4f74
Yavor Grancharov <bearhold@gmail.com>
2018-01-21 18:57:10 +0200

    added folder Linear Data Structures - Stacks and Queues

---
+++ Data-Structures/Linear Data Structures - Stacks and Queues - Exercise/Calculate Sequence/Calculate Sequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Sequence
+{
+    class Program
+    {
+        static void Sequence (int n)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(n);
+            List<int> result = new List<int>();
+
+            while (result.Count < 50)
+            {
+                int current = queue.Dequeue();
+                result.Add(current);
+                queue.Enqueue(current + 1);
+                queue.Enqueue(2 * current + 1);
+                queue.Enqueue(current + 2);
+            }
+            Console.WriteLine(string.Join(", ",result));
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Sequence(n);
+        }
+    }
+}
