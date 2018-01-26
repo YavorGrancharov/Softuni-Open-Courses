9a5fb837192ec3ab8e80627b5e84f022e97fab54
YavorGrancharov <bearhold@gmail.com>
2018-01-26 19:25:59 +0200

    Update Tree.cs

--- Data-Structures/Binary Search Trees/Trees/Tree.cs
+++ Data-Structures/Binary Search Trees/Trees/Tree.cs
@@ -1,18 +1,36 @@
-﻿using System;
+using System;
 
 public class Tree<T>
 {
+    public T Value { get; set; }
+    public IList<Tree<T>> Children { get; set; }
+
     public Tree(T value, params Tree<T>[] children)
     {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+        foreach (Tree<T> child in children)
+        {
+            this.Children.Add(child);
+        }
     }
 
     public void Print(int indent = 0)
     {
-        throw new NotImplementedException();
+        Console.Write(new string(" ", indent * 2));
+        Console.WriteLine(this.Value);
+        foreach (Tree<T> child in this.Children)
+        {
+            child.Print(indent + 1);
+        }
     }
 
     public void Each(Action<T> action)
     {
-        throw new NotImplementedException();
+        action(this.Value);
+        foreach (Tree<T> child in this.Children)
+        {
+            child.Each(action);
+        }
     }
 }
